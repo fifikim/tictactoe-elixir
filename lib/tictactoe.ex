d3d5bc4f69446fe382da1379fe3bc0ba01addafc
Sophia Kim <mail@fifikim.com>
2022-06-09 19:25:14 -0400

    enable pre-commit and pre-push git hooks

--- lib/tictactoe.ex
+++ lib/tictactoe.ex
@@ -13,6 +13,6 @@ defmodule TicTacToe.CLI do
 
   """
   def main(_args) do
-    IO.puts("Welcome to Tic Tac Toe!")
+    "Welcome to Tic Tac Toe!"
   end
 end
---
+++ test/tictactoe_test.exs
@@ -0,0 +1,8 @@
+defmodule TicTacToe.CLITest do
+  use ExUnit.Case
+  doctest TicTacToe.CLI
+
+  test "main returns the Welcome greeting" do
+    assert TicTacToe.CLI.main(1) == "Welcome to Tic Tac Toe!"
+  end
+end
