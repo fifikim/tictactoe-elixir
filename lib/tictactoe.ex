a9ae2f903a8a08489f6c3c6313db86a15381dd1f
Sophia Kim <mail@fifikim.com>
2022-06-09 18:50:30 -0400

    install credo and add to CI

--- lib/tictactoe.ex
+++ lib/tictactoe.ex
@@ -1,4 +1,17 @@
 defmodule TicTacToe.CLI do
+  @moduledoc """
+  Documentation for `TicTacToe.CLI`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TttElixir.hello()
+      :world
+
+  """
   def main(_args) do
     IO.puts("Welcome to Tic Tac Toe!")
   end
