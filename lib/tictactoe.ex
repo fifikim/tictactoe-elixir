71cba34077f718e9159e3b2ea41077c9aa3887d7
Sophia Kim <mail@fifikim.com>
2022-06-09 18:41:15 -0400

    add escript and github actions workflow

---
+++ lib/tictactoe.ex
@@ -0,0 +1,5 @@
+defmodule TicTacToe.CLI do
+  def main(_args) do
+    IO.puts("Welcome to Tic Tac Toe!")
+  end
+end
--- mix.exs
+++ mix.exs
@@ -3,10 +3,11 @@ defmodule TttElixir.MixProject do
 
   def project do
     [
-      app: :ttt_elixir,
+      app: :tictactoe,
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      escript: escript(),
       deps: deps()
     ]
   end
@@ -24,5 +25,9 @@ defmodule TttElixir.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp escript do
+    [main_module: TicTacToe.CLI]
   end
 end
