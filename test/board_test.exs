44158b53a5ff68980a1e7118c1bd230c1cdb6297
Sophia Kim <mail@fifikim.com>
2022-06-13 16:04:14 -0400

    update Board test description

--- test/board_test.exs
+++ test/board_test.exs
@@ -3,7 +3,7 @@ defmodule TicTacToe.BoardTest do
 
   use ExUnit.Case
 
-  test "Board struct creates a struct that contains a key of :cells and value of list [1, 2 3 4 5 6 7 8 9]" do
+  test "Board struct creates a struct that contains {cells: [1, 2, 3, 4, 5, 6, 7, 8, 9]}" do
     board = %Board{}
     assert board.cells == [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
