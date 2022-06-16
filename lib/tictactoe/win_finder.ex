86cf12604b4fe96a7918f481726aa2f32316225b
Sophia Kim <mail@fifikim.com>
2022-06-16 15:02:43 -0400

    add WinFinder helper functions

--- lib/tictactoe/win_finder.ex
+++ lib/tictactoe/win_finder.ex
@@ -2,9 +2,26 @@
 #   alias TicTacToe.Board
 #   alias TicTacToe.WinFinder
 
-#   def game_won?(%Board{} = board, marker) do
-#     combos
-#     |> Enum.any?(&)
+#   def game_won?(%Board{cells: cells} = board, marker) do
+#     get_rows(cells)
 #   end
 
+#   defp get_rows(cells) do
+#     Enum.chunk_every(cells, row_length(cells))
+#   end
+
+#   defp get_columns(cells) do
+
+#   end
+
+#   defp get_diagonals(cells) do
+
+#   end
+
+#   defp row_length(cells) do
+#     cells
+#     |> length()
+#     |> :math.sqrt()
+#     |> trunc()
+#   end
 # end
