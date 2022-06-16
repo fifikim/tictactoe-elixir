65fc844820c1b4d788238b5d0052a337a91a0a6e
Sophia Kim <mail@fifikim.com>
2022-06-16 14:46:08 -0400

    add WinFinder module and tests

---
+++ lib/tictactoe/win_finder.ex
@@ -0,0 +1,10 @@
+# defmodule TicTacToe.WinFinder do
+#   alias TicTacToe.Board
+#   alias TicTacToe.WinFinder
+
+#   def game_won?(%Board{} = board, marker) do
+#     combos
+#     |> Enum.any?(&)
+#   end
+
+# end
